1401f730aa5ab190a347b6a51521e3017a4726a4
shuhrat567 <isoilovshuhrat567@gmail.com>
2023-08-16 11:29:25 +0300

    javasicript

---
+++ js/app.js
@@ -0,0 +1,32 @@
+'use strict'
+
+// function salomlashish() {
+//     console.log("Salom Dunyo");
+// }
+
+
+// salomlashish();
+
+
+
+
+// function user(name) {
+//     name='Kamil'
+//     console.log(name);
+   
+// }
+// user();
+
+
+
+
+// function userQuestion(){
+//     let askName = prompt("Ismingiz nima");
+//     let askAge = +prompt("Yoshingizni kiriting");
+//     let askAddress = prompt("Manzilingizni kiriting");
+//     console.log(askName); 
+//     console.log(askAge);
+//      console.log(askAddress); 
+// }
+// userQuestion();
+
---
+++ js/callback.js
@@ -0,0 +1,30 @@
+'use strict'
+
+// function salomlashish(text){
+//     console.log(`salom ${text}`);
+// }
+
+// salomlashish()
+
+
+
+// function callback1(name, callback){
+//     console.log(`Salom ${name}`);
+//     callback()
+// }
+// function callback(){
+//     console.log("Callback funksiya ishladi");
+// }
+
+// callback1("Tommy",callback)
+
+
+// function greet(){
+//     console.log("Salom Dunyo");
+// }
+// function sayName(name){
+//     setTimeout(()=>{
+//         greet()
+//     } ,2000)
+// }
+// sayName("Jacob",greet)
